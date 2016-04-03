2ea4bf98cc5486922b5e3206ad3a9fbbee30b718
Daniel Martínez <danielmartinezalmela@gmail.com>
2016-04-03 11:21:56 +0200

    Actualización a MySQL 5.7

--- _vagrant/config.sh
+++ _vagrant/config.sh
@@ -2,10 +2,10 @@
 
 # Variables y funciones.
 
-PASSWORD='123' 								       # Contraseña de la base de datos.
-DATABASE_SQL='database.sql' 				       # Base de datos.
-PROJECTFOLDER='proyecto' 					  	   # Nombre del directorio del proyecto.
-GIT_REPOS='https://github.com/luiscavero92/myMini' # Repositorios del proyecto en GitHub.
+DATABASE_SQL='database.sql' 				             # Base de datos.
+PASSWORD='123' 								       		 # Contraseña de la base de datos.
+PROJECTFOLDER='proyecto' 					  	   		 # Nombre del directorio del proyecto.
+GIT_REPOS='https://github.com/DanMnez/Vagrant-LAMP-PHP7' # Repositorios del proyecto en GitHub.
 
 update() {
 	sudo apt-get update
@@ -27,38 +27,20 @@ update
 sudo apt-get install php7.0-cli php7.0 libapache2-mod-php7.0 php7.0-mysql -y
 sudo apt-get install php7.0-cgi php7.0-dbg php7.0-dev php7.0-curl php7.0-gd -y
 sudo apt-get install php7.0-mcrypt php7.0-xsl php7.0-intl -y
-sudo apt-get install php7.0-opcache -y 
-
-
-# Instalación de mysql
-echo -e "\n\n===== Instalando mysql: =====\n\n" 
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-
-# Instalación de phpmyadmin
-echo -e "\n\n===== Instalando phpmyadmin: =====\n\n" 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install -y phpmyadmin
-
 
 # Instalación de mysql 5.7
 # Forzamos al sistema a no tener interaccion manual.
-#export DEBIAN_FRONTEND=noninteractive
-#apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5 
-#echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
-#update
+export DEBIAN_FRONTEND=noninteractive
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5 
+echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
+update
 
-#debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
-#debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $PASSWORD"
-#debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $PASSWORD"
-#sudo apt-get install mysql-server -y
+debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $PASSWORD"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $PASSWORD"
+sudo apt-get install mysql-server -y
 
-# Instalacion de administración de BBDD
+# Instalacion de administración de BBDD [Proximo]
 
 
 # Creación del directorio del proyecto
