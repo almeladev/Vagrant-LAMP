93e4c87c9ff4f55cf8fef711233c1812c4b6ee5f
Daniel Martínez <danielmartinezalmela@gmail.com>
2016-04-03 11:12:52 +0200

    Removidos antiguos archivos

--- _vagrant/config.sh
+++ _vagrant/config.sh
@@ -1,49 +1,40 @@
 #!/usr/bin/env bash
 
-# Use single quotes instead of double quotes to make it work with special-character passwords
+# Variables y funciones.
 
-# Contraseña de la base de datos
-PASSWORD='123'
+PASSWORD='123' 								       # Contraseña de la base de datos.
+DATABASE_SQL='database.sql' 				       # Base de datos.
+PROJECTFOLDER='proyecto' 					  	   # Nombre del directorio del proyecto.
+GIT_REPOS='https://github.com/luiscavero92/myMini' # Repositorios del proyecto en GitHub.
 
-# Nombre del archivo sql de la base de datos (debe encontrarse en la carpeta _install)
-DATABASE_SQL='database.sql'
+update() {
+	sudo apt-get update
+}
 
-# Directorio del proyecto (donde se instalarán los repositorios de github)
-PROJECTFOLDER='myproject'
+# Actualizamos el sistema e instalamos utilidades
+update
+sudo apt-get upgrade -y
+sudo apt-get install zip unzip -y
 
-# Repositorios propios, puedes poner la url de tu propio repositorio github
-GIT_REPOS='https://github.com/luiscavero92/myMini'
-# Cambiar por el de abajo para instalar el mini original
-# GIT_REPOS = 'https://github.com/panique/mini'
-
-sudo apt-get update
-sudo apt-get -y upgrade
 # Instalación de apache2 (-y para no pedir confirmación)
-echo -e "\n\n ===== Instalando Apache2: =====\n\n" 
-sudo apt-get install -y apache2
+sudo apt-get install apache2 -y
 
-# Actualización e instalación de php 5.6
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php5-5.6
-sudo apt-get update
-sudo apt-get install -y php5
+# Actualización e instalación de php 7 
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/php
+update
 
-# Actualización e instalación de php 5.6
-echo -e "\n\n ===== Instalando php5: =====\n\n" 
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php5-5.6
-sudo apt-get update
-sudo apt-get install -y php5
-echo -e "\n\n===== Version de php: =====\n\n" 
-php5 -v
-echo -e "\n\n=========================\n\n"
+sudo apt-get install php7.0-cli php7.0 libapache2-mod-php7.0 php7.0-mysql -y
+sudo apt-get install php7.0-cgi php7.0-dbg php7.0-dev php7.0-curl php7.0-gd -y
+sudo apt-get install php7.0-mcrypt php7.0-xsl php7.0-intl -y
+sudo apt-get install php7.0-opcache -y 
+
 
 # Instalación de mysql
 echo -e "\n\n===== Instalando mysql: =====\n\n" 
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get -y install mysql-server
-sudo apt-get install -y php5-mysql
 
 # Instalación de phpmyadmin
 echo -e "\n\n===== Instalando phpmyadmin: =====\n\n" 
@@ -54,10 +45,26 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install -y phpmyadmin
 
+
+# Instalación de mysql 5.7
+# Forzamos al sistema a no tener interaccion manual.
+#export DEBIAN_FRONTEND=noninteractive
+#apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5 
+#echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
+#update
+
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $PASSWORD"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $PASSWORD"
+#sudo apt-get install mysql-server -y
+
+# Instalacion de administración de BBDD
+
+
 # Creación del directorio del proyecto
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
 
-# Creación del host
+# Creacion del virtualhost
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
     DocumentRoot "/var/www/html/${PROJECTFOLDER}/public"
@@ -89,7 +96,7 @@ sudo git clone "${GIT_REPOS}" "/var/www/html/${PROJECTFOLDER}"
 
 # Instalación de composer
 echo -e "\n\n===== Instalando Composer: =====\n\n" 
-curl -s https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Instalación de los paquetes incluidos en composer
@@ -103,6 +110,3 @@ sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFO
 
 # Copia la contraseña de la base de datos en el archivo de configuración -> define('DB_PASS', 'your_password');
 sudo sed -i "s/your_password/${PASSWORD}/" "/var/www/html/${PROJECTFOLDER}/application/config/config.php"
-
-# Mensaje final
-echo -e "\n\n===== Voila =====\n\n" 
