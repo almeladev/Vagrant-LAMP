816f0c305225c83bdd130232782184adc41cd61f
Daniel Martínez <danielmartinezalmela@gmail.com>
2016-04-04 05:33:26 +0200

    Mejorados los comentarios
    Separadas las secciones de configuración y se ha añadido una cabecera
    informativa al script

--- _vagrant/config.sh
+++ _vagrant/config.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
 
-# Variables y funciones.
+# ======================================================= #
+# config.sh
+#
+# Archivo de configuración automática para la box 
+# Ubuntu 14.04
+#
+# Autor: Daniel Martínez
+# Author URI: http://danmnez.me
+# Version: 0.7
+# Licencia: http://unlicense.org/
+# ======================================================= #
 
-DATABASE_SQL='database.sql' 				             # Base de datos.
-PASSWORD='123' 								       		 # Contraseña de la base de datos.
-PROJECTFOLDER='proyecto' 					  	   		 # Nombre del directorio del proyecto.
-GIT_REPOS='https://github.com/DanMnez/Vagrant-LAMP-PHP7' # Repositorios del proyecto en GitHub.
+# ======================================================= #
+# Variables y funciones globales
+# ======================================================= #
+
+DATABASE_SQL='database.sql' 				              # Base de datos.
+PASSWORD='123' 								       		  # Contraseña de la base de datos.
+PROJECTFOLDER='proyecto' 					  	   		  # Nombre del directorio del proyecto.
+GIT_REPOS='https://github.com/DanMnez/Vagrant-LAMP-PHP7'  # Repositorios del proyecto en GitHub.
 
 update() {
 	sudo apt-get update
 }
 
+# ======================================================= #
 # Actualizamos el sistema e instalamos utilidades
+# ======================================================= #
 update
 sudo apt-get upgrade -y
 sudo apt-get install zip unzip -y
 
-# Instalación de apache2 (-y para no pedir confirmación)
+# ======================================================= #
+# Instalación de apache2
+# ======================================================= #
 sudo apt-get install apache2 -y
 
-# Actualización e instalación de php 7 
+# ======================================================= #
+# Instalación de php 7 
+# ======================================================= #
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:ondrej/php
 update
@@ -28,9 +48,10 @@ sudo apt-get install php7.0-cli php7.0 libapache2-mod-php7.0 php7.0-mysql -y
 sudo apt-get install php7.0-cgi php7.0-dbg php7.0-dev php7.0-curl php7.0-gd -y
 sudo apt-get install php7.0-mcrypt php7.0-xsl php7.0-intl -y
 
-# Instalación de mysql 5.7
-# Forzamos al sistema a no tener interaccion manual.
-export DEBIAN_FRONTEND=noninteractive
+# ======================================================= #
+# Instalación de mysql 5.7 
+# ======================================================= #
+export DEBIAN_FRONTEND=noninteractive # Forzamos al sistema a no tener interacción manual.
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5 
 echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
 update
@@ -40,10 +61,14 @@ debconf-set-selections <<< "mysql-community-server mysql-community-server/root-p
 debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $PASSWORD"
 sudo apt-get install mysql-server -y
 
-# Creación del directorio del proyecto
+# ======================================================= #
+# Creación del directorio del proyecto 
+# ======================================================= #
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
 
-# Creacion del virtualhost
+# ======================================================= #
+# Creación del virtualhost
+# ======================================================= #
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
     DocumentRoot "/var/www/html/${PROJECTFOLDER}/public"
@@ -56,40 +81,53 @@ EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
+# ======================================================= #
 # Habilita el módulo reescritura para urls amigables
-sudo a2enmod rewrite
-
 # Reinicia apache
+# ======================================================= #
+sudo a2enmod rewrite
 service apache2 restart
 
+# ======================================================= #
 # Elimina el index.html por defecto de apache
+# ======================================================= #
 sudo rm "/var/www/html/index.html"
 
-# Instala git
-echo -e "\n\n===== Instalando Git: =====\n\n" 
+# ======================================================= #
+# Instala Git
+# ======================================================= #
 sudo apt-get install -y git
 
-# Descarga mis repositorios de git en el directorio del proyecto
-echo -e "\n\n===== Clonando repositorios Git: =====\n\n" 
+# ======================================================= #
+# Clona el repositorio de GitHub
+# ======================================================= #
 sudo git clone "${GIT_REPOS}" "/var/www/html/${PROJECTFOLDER}"
 
-# Descarga el administrador de MySQL Adminer en su ultima version
+# ======================================================= #
+# Descarga el administrador de MySQL Adminer
+# ======================================================= #
 sudo mkdir "/var/www/html/${PROJECTFOLDER}/public/adminer"
 sudo wget "http://www.adminer.org/latest.php" -O "/var/www/html/${PROJECTFOLDER}/public/adminer/index.php" 
 
-# Instalación de composer
-echo -e "\n\n===== Instalando Composer: =====\n\n" 
+# ======================================================= #
+# Instalación de Composer
+# ======================================================= #
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
+# ======================================================= #
 # Instalación de los paquetes incluidos en composer
-echo -e "\n\n===== Instalando paquetes de Composer: =====\n\n" 
+# ======================================================= #
 cd "/var/www/html/${PROJECTFOLDER}"
 composer install
 
-# Carga la base de datos a partir del archivo database.sql
-echo -e "\n\n===== Cargando la base de datos: =====\n\n" 
+# ======================================================= #
+# Carga la base de datos
+# ======================================================= #
 sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/${DATABASE_SQL}"
 
-# Copia la contraseña de la base de datos en el archivo de configuración -> define('DB_PASS', 'your_password');
+# ======================================================= #
+# Copia la contraseña de la base de datos en el archivo 
+# de configuración -> define('DB_PASS', 'your_password');
+# ======================================================= #
 sudo sed -i "s/your_password/${PASSWORD}/" "/var/www/html/${PROJECTFOLDER}/application/config/config.php"
