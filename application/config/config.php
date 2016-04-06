b41e0b5e289c3a9872687bdf4e55d111fff0807e
Daniel Martínez <danielmartinezalmela@gmail.com>
2016-04-06 22:19:56 +0200

    Traducción de comentarios

--- application/config/config.php
+++ application/config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Configuration
+ * Configuración de la App.
  *
- * For more info about constants please @see http://php.net/manual/en/function.define.php
+ * @see http://php.net/manual/en/function.define.php
  */
 
 /**
- * Configuration for: Error reporting
- * Useful to show every little problem during development, but only show hard errors in production
+ * Informe de errores.
+ * útil para mostrar los pequeños problemas durante el desarrollo.
  */
 define('ENVIRONMENT', 'development');
 
@@ -18,26 +18,22 @@ if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
 }
 
 /**
- * Configuration for: URL
- * Here we auto-detect your applications URL and the potential sub-folder. Works perfectly on most servers and in local
- * development environments (like WAMP, MAMP, etc.). Don't touch this unless you know what you do.
+ * Configuración de la URL.
  *
- * URL_PUBLIC_FOLDER:
- * The folder that is visible to public, users will only have access to that folder so nobody can have a look into
- * "/application" or other folder inside your application or call any other .php file than index.php inside "/public".
+ * URL_PUBLIC_FOLDER: 
+ * Se trata de la carpeta pública, a la cual tienen acceso todo el mundo. No ocurre lo mismo con /application 
  *
  * URL_PROTOCOL:
- * The protocol. Don't change unless you know exactly what you do.
+ * El tipo de protocolo a usar.
  *
  * URL_DOMAIN:
- * The domain. Don't change unless you know exactly what you do.
+ * El dominio (la IP) que estamos usando.
  *
  * URL_SUB_FOLDER:
- * The sub-folder. Leave it like it is, even if you don't use a sub-folder (then this will be just "/").
+ * Las subcarpetas.
  *
  * URL:
- * The final, auto-detected URL (build via the segments above). If you don't want to use auto-detection,
- * then replace this line with full URL (and sub-folder) and a trailing slash.
+ * Es la URL final, combinando las demás constantes (protocolo, dominio y subcarpetas).
  */
 
 define('URL_PUBLIC_FOLDER', 'public');
@@ -47,8 +43,8 @@ define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SC
 define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
 
 /**
- * Configuration for: Database
- * This is the place where you define your database credentials, database type etc.
+ * Configuración de la base de datos.
+ * Definimos las constantes para la base de datos (tipo, host, nombre, usuario, contraseña y codificación).
  */
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
